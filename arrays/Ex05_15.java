827e90ac8a9e2fa5273169a39e8173f4fdebccff
kexxxon <dev.kexon@gmail.com>
2022-09-14 03:05:04 +0900

    String to morse

---
+++ arrays/Ex05_15.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Ex05_15 {
+    public static void main(String[] args) {
+        String src = "SOSHELP";
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                          "....", "..", ".---", "-.-", ".-..", "--", "-.",
+                          "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                          "...-", ".--", "-..-", "-.--", "--.."};
+
+        String result = "";
+
+        for(int i = 0; i < src.length(); i++) {
+            result += morse[src.charAt(i) - 'A'];
+        }
+
+        System.out.println("src = " + src);
+        System.out.println("morse = " + result);
+    }
+}
